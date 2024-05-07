d9a5d650bd913289a64108283f1dce9d10097e6c
NanoHtz <fernandogalvez16@gmail.com>
2024-05-07 14:16:53 +0200

    avance

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fgalvez- <fgalvez-@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: fgalvez- <fgalvez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 19:08:30 by fgalvez-          #+#    #+#             */
-/*   Updated: 2024/04/26 19:08:30 by fgalvez-         ###   ########.fr       */
+/*   Updated: 2024/05/07 14:13:42 by fgalvez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,28 @@ Debe tratar algunos errores:
 Archivos vacios, lineas vacias y errores de lectura
 */
 #include "get_next_line.h"
+#include <Libft.h>
 
 #define BUFFER_SIZE 1024 //Definimos una macro que guarde el tamaño del buffer.
 
-char *get_next_line(int fd) //Archivo ya abierto, no tenemos que abrirlo nosotros.
+char *get_next_line(int fd) //Archivo ya abierto, no tenemos que abrirlo nosotros. Lo abrimos desde el main.
 {
 	static char *buffer; /*Una variable estática, mantiene su valor entre las llamadas
 	y que guarda el contenido del archivo,
 	esto permite a la función recordar donde estaba.*/
+	char *newline; //Variable que guarda la posición del salto de línea.
+
 	if (!buffer)
 	{
-		/* code */
+		buffer = malloc(BUFFER SIZE); //Reservamos memoria para el buffer y comprobamos.
+		if (!buffer)
+			return (NULL);
+	}
+
+	//Leemos el archivo y guardamos el contenido en el Buffer.
+	while (newline = ft_strchr(buffer, '\n') == NULL) //Busca un salto de línea en el buffer.
+	{
+		read(fd, buffer, BUFFER_SIZE);
 	}
 	return (0);
 }
