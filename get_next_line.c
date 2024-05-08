491e907e8540cda800979c3b686c22f658ffece7
NanoHtz <fgalvez.g26@gmail.com>
2024-05-08 11:13:47 +0200

    get final

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fgalvez- <fgalvez-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fgalvez- <fgalvez-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 19:08:30 by fgalvez-          #+#    #+#             */
-/*   Updated: 2024/05/07 14:13:42 by fgalvez-         ###   ########.fr       */
+/*   Updated: 2024/04/26 19:08:30 by fgalvez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,30 +21,53 @@ Debe ser compatible en multiples sistemas operativos
 Debe tratar algunos errores:
 Archivos vacios, lineas vacias y errores de lectura
 */
+
+/*
+ 1-No tenemos que abrir el archivo, puesto que el parametro es un descriptor de archivi
+ se trabaja con un archivo ya abierto.
+ 2-Asignar buffer y hacer la comprobación
+ 3-Leer el archivo hasta \n o 'EOF'.
+ 4-Devolver la linea leida o NULL si no hay nada que leer o seguir leyendo.????
+*/
 #include "get_next_line.h"
-#include <Libft.h>
 
 #define BUFFER_SIZE 1024 //Definimos una macro que guarde el tamaño del buffer.
 
-char *get_next_line(int fd) //Archivo ya abierto, no tenemos que abrirlo nosotros. Lo abrimos desde el main.
+char *get_next_line(int fd) //Archivo ya abierto, no tenemos que abrirlo nosotros.
 {
 	static char *buffer; /*Una variable estática, mantiene su valor entre las llamadas
 	y que guarda el contenido del archivo,
 	esto permite a la función recordar donde estaba.*/
-	char *newline; //Variable que guarda la posición del salto de línea.
-
-	if (!buffer)
+	ssize_t b_read; //Esta variable guarda el número de bytes leido, es un tamaño entero con signo.
+	char *new_pos; //Guarda la posición de la siguiente línea.
+	char *line; //Guarda la línea leida.
+	if (!buffer) // Comprueba si el buffer ha sido asignado, es una protección.
 	{
-		buffer = malloc(BUFFER SIZE); //Reservamos memoria para el buffer y comprobamos.
-		if (!buffer)
-			return (NULL);
+		buffer = malloc (BUFFER_SIZE); //Reservamos memoria para el buffer.
+		if (!buffer) //Volvemos a realizar la comprobación.
+			return NULL;
 	}
 
-	//Leemos el archivo y guardamos el contenido en el Buffer.
-	while (newline = ft_strchr(buffer, '\n') == NULL) //Busca un salto de línea en el buffer.
+	b_read = read(fd, buffer, BUFFER_SIZE);//Lee el archivo y lo guarda en b_read
+	if (b_read == -1) //Comprobamos si hay un error en la lectura.
 	{
-		read(fd, buffer, BUFFER_SIZE);
+		free(buffer); //Liberamos la memoria del buffer.
+		return NULL;
 	}
-	return (0);
+
+	buffer[b_read] = '\0'; //En la última posición del buffer añadimos el carcater del final para poder tratarlo como una cadena de caracteres.
+	new_pos = ft_strchr(buffer, '\n');
+	if (!new_pos)
+		new_pos = ft_strchr(buffer, '\0');
+
+	*new_pos = '\0'; //Añadimos el caracter al final de la línea para poder usar strdup
+
+	line = ft_strdup(buffer); //Guardamos la línea en la variable line.
+	if (!line)
+		return (NULL);
+	ft_memmove(buffer, new_pos + 1, ft_strlen(new_pos + 1) + 1); //Movemos el buffer para que la siguiente llamada a la función pueda leer la siguiente línea.
+
+	return (line);
 }
+
 
